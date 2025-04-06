c90040cd42b9149f3f7bda00acdcd7eff6c52b6d
Luis Espino <66042898+luisespino@users.noreply.github.com>
2025-04-06 03:41:16 -0600

    Update mlearn.mjs

--- mlearn.mjs
+++ mlearn.mjs
@@ -1,44 +1,44 @@
 export const LinearRegression = async () => {
-    const { LinearRegression } = await import('/linear-model.mjs');
+    const { LinearRegression } = await import('./linear-model.mjs');
     return LinearRegression;
 };
 
 export const PolynomialRegression = async () => {
-    const { PolynomialRegression } = await import('/linear-model.mjs');
+    const { PolynomialRegression } = await import('./linear-model.mjs');
     return PolynomialRegression;
 };
 
 export const GaussianNB = async () => {
-    const { GaussianNB } = await import('/naive-bayes.mjs');
+    const { GaussianNB } = await import('./naive-bayes.mjs');
     return GaussianNB;
 };
 
 export const DecisionTreeClassifier = async () => {
-    const { DecisionTreeClassifier } = await import('/tree.mjs');
+    const { DecisionTreeClassifier } = await import('./tree.mjs');
     return DecisionTreeClassifier;
 };
 
 export const LabelEncoder = async () => {
-    const { LabelEncoder } = await import('/preprocessing.mjs');
+    const { LabelEncoder } = await import('./preprocessing.mjs');
     return LabelEncoder;
 };
 
 export const trainTestSplit = async () => {
-    const { trainTestSplit } = await import('/model-selection.mjs');
+    const { trainTestSplit } = await import('./model-selection.mjs');
     return trainTestSplit;
 };
 
 export const joinArrays = async () => {
-    const { joinArrays } = await import('/model-selection.mjs');
+    const { joinArrays } = await import('./model-selection.mjs');
     return joinArrays;
 };
 
 export const zip = async () => {
-    const { zip } = await import('/model-selection.mjs');
+    const { zip } = await import('./model-selection.mjs');
     return zip;
 };
 
 export const accuracyScore = async () => {
-    const { accuracyScore } = await import('/metrics.mjs');
+    const { accuracyScore } = await import('./metrics.mjs');
     return accuracyScore;
 };
